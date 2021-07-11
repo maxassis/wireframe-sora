ca726d816dc63ea246ea6d47b80c17fc5823bd6d
max <max.assis@ymail.com>
2021-07-11 16:20:25 -0300

    pequenos ajustes.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import vector from "../../assets/imgs/Vector.svg";
-import img1 from "../../assets/imgs/img1.jpg";
-import img2 from "../../assets/imgs/img2.jpg";
-import img3 from "../../assets/imgs/img3.jpg";
+import img1 from "../../assets/imgs/img1.png";
+import img2 from "../../assets/imgs/img2.png";
+import img3 from "../../assets/imgs/img3.png";
 
 export default function Hero() {
   return (
     <section className="container">
-
       <div className={styles["wrapper-main"]}>
-      
         <div className={styles["wrapper-titles"]}>
           <span>Avaliable</span>
           <h1>Wireframe Sora</h1>
@@ -25,11 +23,10 @@ export default function Hero() {
         </div>
 
         <div className={styles["wrapper-image"]}>
-          <img src={img1} alt="img1" className={styles.img1}/>
-          <img src={img2} alt ="img2" className={styles.img2} />
-          <img src={img3} alt ="img3" />
+          <img src={img1} alt="img1" className={styles.img1} />
+          <img src={img2} alt="img2" className={styles.img2} />
+          <img src={img3} alt="img3" />
         </div>
-      
       </div>
     </section>
   );
